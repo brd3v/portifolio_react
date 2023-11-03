646c86b77a375264a75eb888c33d3bddafe38adc
brd3v <bnsiq2015@gmail.com>
2023-11-03 00:28:08 -0300

    ajustes final

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ function App() {
         <div className='col-md-4'><img src="https://raw.githubusercontent.com/brd3v/portifolio_react/main/public/images/anime%20news.png" alt="" /> <h5>Portal de notícias</h5> <p>Aplicação utilizando Ajax, React, bootstrap</p> 
         <a className='link' target='blank' href="https://github.com/brd3v/MangaNews">Código</a> 
         <a className='link' target='blank' href="https://manga-news.vercel.app/">Deploy</a></div>
-        <div className='col-md-4'><img src="../public/images/papgina de venda.png" alt="" /> <h5>Pafgina de vendas</h5><p>Pagina de vendas desenvolvida para infoprodutores</p>
+        <div className='col-md-4'><img src="https://github.com/brd3v/portifolio_react/blob/main/public/images/papgina%20de%20venda.png?raw=true" alt="" /> <h5>Pafgina de vendas</h5><p>Pagina de vendas desenvolvida para infoprodutores</p>
         <a className='link' target='blank' href="https://github.com/brd3v/site_vendas">Código</a>
-        <a className='link' target='blank' href="https://github.com/brd3v/portifolio_react/blob/main/public/images/papgina%20de%20venda.png">Deploy</a></div>
+        <a className='link' target='blank' href="https://site-vendas-seven.vercel.app/">Deploy</a></div>
         <div className='col-md-4'><img src="https://github.com/brd3v/portifolio_react/blob/main/public/images/site%20advogado.png?raw=true" alt="" /><h5>Landingpage + Fromulario</h5><p>Projeto Feito com Javascript + Bootstrap</p>
         <a className='link' target='blank' href="https://github.com/brd3v/sites">Código</a>
         <a className='link' href="sites-black.vercel.app">Deploy</a></div>
