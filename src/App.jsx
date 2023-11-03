e8c9c8535bc9403c4a3d2c72bf928b84c3a6ab39
brd3v <bnsiq2015@gmail.com>
2023-11-03 00:20:52 -0300

    corracao e otimização

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,16 @@ function App() {
     <div className='projects'>
       <h3>Projetos</h3>
       <div className='row'>
-        <div className='col-md-4'><img src="/public/images/termometro.png" alt="" /> <h5>Termometro</h5><p>Projeto Feito com javascript + Reactjs + ajax +  Bootstrap + react icons</p>
+        <div className='col-md-4'><img src="../public/images/termometro.png" alt="" /> <h5>Termometro</h5><p>Projeto Feito com javascript + Reactjs + ajax +  Bootstrap + react icons</p>
         <a className='link' target='blank' href="https://github.com/brd3v/temperatura">Código</a>
         <a className='link' target='blank' href="https://temperatura-ecru.vercel.app/">Deploy</a></div>
-        <div className='col-md-4'><img src="/public/images/anime news.png" alt="" /> <h5>Portal de notícias</h5> <p>Aplicação utilizando Ajax, React, bootstrap</p> 
+        <div className='col-md-4'><img src="../public/images/anime news.png" alt="" /> <h5>Portal de notícias</h5> <p>Aplicação utilizando Ajax, React, bootstrap</p> 
         <a className='link' target='blank' href="https://github.com/brd3v/MangaNews">Código</a> 
         <a className='link' target='blank' href="https://manga-news.vercel.app/">Deploy</a></div>
-        <div className='col-md-4'><img src="/public/images/papgina de venda.png" alt="" /> <h5>Pafgina de vendas</h5><p>Pagina de vendas desenvolvida para infoprodutores</p>
+        <div className='col-md-4'><img src="../public/images/papgina de venda.png" alt="" /> <h5>Pafgina de vendas</h5><p>Pagina de vendas desenvolvida para infoprodutores</p>
         <a className='link' target='blank' href="https://github.com/brd3v/site_vendas">Código</a>
         <a className='link' target='blank' href="https://site-vendas-seven.vercel.app/">Deploy</a></div>
-        <div className='col-md-4'><img src="/public/images/site advogado.png" alt="" /><h5>Landingpage + Fromulario</h5><p>Projeto Feito com Javascript + Bootstrap</p>
+        <div className='col-md-4'><img src="../public/images/site advogado.png" alt="" /><h5>Landingpage + Fromulario</h5><p>Projeto Feito com Javascript + Bootstrap</p>
         <a className='link' target='blank' href="https://github.com/brd3v/sites">Código</a>
         <a className='link' href="sites-black.vercel.app">Deploy</a></div>
 
